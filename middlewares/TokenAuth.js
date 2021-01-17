222c922b50a68e8e5ec077d200a9b98b10457a79
Abdallah Gamal <abdoolly@gmail.com>
2021-01-17 12:33:45 +0200

    small fix in tokenAuth

--- middlewares/TokenAuth.js
+++ middlewares/TokenAuth.js
@@ -1,6 +1,6 @@
 const Token = require("../models/Token");
 
-const TokenAuth = (req, res, next) => {
+const TokenAuth = async (req, res, next) => {
     if (!req.headers.API_TOKEN) {
         return res.status(401).send({ error: `You are not authorized to make this request` });
     }
