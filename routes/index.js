65f77d90d1241ad11b23ae0ee120d31a82206db5
Abdallah Gamal <abdoolly@gmail.com>
2021-01-17 13:17:42 +0200

    implemented createContact with validation

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const Contact = require('../models/Contact');
+const { throwOnValidationError } = require('../services/Helpers');
+const { CreateContactValidator } = require('../services/Validators');
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-    return res.send('hello world');
+router.post('/contact', CreateContactValidator(), async (req, res) => {
+    const { name, phoneNumbers, email, mailingAddress } = req.body;
+    await throwOnValidationError(req);
+    const contact = await Contact.create({
+        name,
+        phoneNumbers,
+        email,
+        mailingAddress
+    });
+    return res.send(contact);
 });
 
 module.exports = router;
