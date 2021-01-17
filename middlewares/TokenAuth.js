543716b4c56eba7b9196ea8275c00b7c86ad0be7
Abdallah Gamal <abdoolly@gmail.com>
2021-01-17 12:33:04 +0200

    added TokenAuth middleware

---
+++ middlewares/TokenAuth.js
@@ -0,0 +1,16 @@
+const Token = require("../models/Token");
+
+const TokenAuth = (req, res, next) => {
+    if (!req.headers.API_TOKEN) {
+        return res.status(401).send({ error: `You are not authorized to make this request` });
+    }
+
+    const token = await Token.findOne({ token: req.headers.API_TOKEN });
+    if (!token) {
+        return res.status(401).send({ error: `You are not authorized to make this request` });
+    }
+
+    return next();
+};
+
+module.exports = TokenAuth;
